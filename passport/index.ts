d3a208dfacf5783b0779abc72203f5fd20705c15
akat32 <baduk32127@gmail.com>
2019-04-02 21:46:40 +0900

    wish Change Source

--- passport/index.ts
+++ passport/index.ts
@@ -12,23 +12,21 @@ passport.serializeUser<any, any>((user, done)=>{
 
 passport.deserializeUser((id, done)=>{
     Users.findById(id, (err, user)=>{
-        done(err, user);
+        done(err, user)
     })
 })
 
 passport.use(new LocalStrategy({
         usernameField: 'id',
         passwordField: 'passwd',
-        session: true,
-        passReqToCallback: false 
+        session: true, // 세션에 저장 여부
+        passReqToCallback: false,
     }, async (id, passwd, done)=>{
-        const user = await Users.findOne({
+        const user = Users.findOne({
             id: id,
             passwd: passwd
         })
         if(user) return done(null, user)
-        else return done(null, false, {message: "아이디나 비밀번호가 틀렸습니다!"})
+        else return done(null, false, {message: "아이디나 비밀번호가 틀렸습니다"})
     })
 )
-
-// req.isAuthenticated() 로그인 확인 함수
--- src/App.ts
+++ src/App.ts
@@ -3,6 +3,11 @@ import { auth } from '../routes/auth'
 
 import * as cors from 'cors';
 import * as bodyParser from 'body-parser'
+import { Users } from '../mongo/index'
+import * as session from 'express-session'
+import passport from 'passport'
+
+import * as passportConfig from '../passport/index'
 
 class App {
   public app: express.Application;
@@ -17,7 +22,13 @@ class App {
   }
   constructor () {
     this.app = express();
-
+    this.app.use(session({
+      secret: 'qwldhcosahdfeshdfiudshfiudshfioudshfoiuashdviuhrsdsadcoiuhdsoiufhoiwusdhfoiuwedshfoiwesudhfocwiesaudhfvokalsudbvkjdscxbvkildshvpwosidzhvpbiu', 
+      resave: true, 
+      saveUninitialized: false
+    }))
+    this.app.use(passport.initialize())
+    this.app.use(passport.session())
     this.app.use(cors())
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({limit: '1gb', extended: false }));
